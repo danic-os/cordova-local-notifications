29edb4ff4ec74e355befa4f488abd9ee85c2bcac
danic-os <daniel.conceicao@outsystems.com>
2025-05-29 15:23:31 +0100

    Update LocalNotifications.swift

--- src/ios/LocalNotifications.swift
+++ src/ios/LocalNotifications.swift
@@ -6,6 +6,33 @@ class LocalNotifications: CDVPlugin {
     @objc(coolMethod:)
     func coolMethod(command: CDVInvokedUrlCommand) {
         print("Something GOES here")
+
+        sendPluginResult(status: .ok, message: "It ran", callbackId: command.callbackId)
+    }
+
+    func sendPluginResult(status: CDVCommandStatus, message: String = "", callbackId: String, keepCallback: Bool = false) {
+        let pluginResult = CDVPluginResult(status: status, messageAs: message)
+        pluginResult?.setKeepCallbackAs(keepCallback)
+        self.commandDelegate!.send(pluginResult, callbackId: callbackId)
     }
 
 }
+
+/*
+@objc(LocalNotifications)
+class LocalNotifications: CDVPlugin {
+    
+    @objc(coolMethod:)
+    func coolMethod(command: CDVInvokedUrlCommand) {
+        print("Cool method called")
+        
+        sendPluginResult(status: .ok, message: "Everything went ok", callbackId: command.callbackId)
+    }
+    
+    func sendPluginResult(status: CDVCommandStatus, message: String = "", callbackId: String, keepCallback: Bool = false) {
+        let pluginResult = CDVPluginResult(status: status, messageAs: message)
+        pluginResult?.setKeepCallbackAs(keepCallback)
+        self.commandDelegate!.send(pluginResult, callbackId: callbackId)
+    }
+}
+*/
